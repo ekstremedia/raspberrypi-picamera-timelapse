19ca3e0d2d51fe0c2060f97901079397b62f58b8
Terje Nesthus <terje@ekstremedia.no>
2023-06-11 03:32:26 +0200

    Cleanup

--- old/original.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python
-from picamera2 import Picamera2
-import time
-picam2 = Picamera2()
-#picam2.options["quality"] = 95
-#picam2.options["compress_level"] = 2
-picam2.start()
-time.sleep(1)
-picam2.capture_file("/var/www/html/test2.jpg")
--- old/start_and_capture.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python
-
-from picamera2 import Picamera2
-picam2 = Picamera2()
-filename = "/var/www/html/test.jpg"
-picam2.start_and_capture_file(filename,delay=0,show_preview=False)
-
