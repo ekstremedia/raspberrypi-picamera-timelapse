0817887ab4895157ee870db101b3e8ffee4cde6f
Terje Nesthus <terje@ekstremedia.no>
2023-09-16 01:20:01 +0200

    Fix night time transition to day

--- capture_image_night.py
+++ capture_image_night.py
@@ -13,36 +13,60 @@ from prettytable import PrettyTable
 from termcolor import colored
 import argparse
 
-# Constants
-DAYTIME_SHUTTER = 4489
-DAYTIME_GAIN = 1
-MAX_SHUTTER = 30000000
-MAX_GAIN = 32
-SUNRISE_OFFSET_MINUTES = -60  # Start transition 1 hour before actual sunrise
-SUNSET_OFFSET_MINUTES = -60
-
-def is_within_transition_period(sunrise_time, sunset_time):
-    sunrise = parser.parse(sunrise_time) + datetime.timedelta(minutes=SUNRISE_OFFSET_MINUTES)
-    sunset = parser.parse(sunset_time)
-    now = datetime.datetime.now()
-    return sunset <= now <= sunset + datetime.timedelta(hours=2) or sunrise <= now <= parser.parse(sunrise_time)
-
-def is_hour_before_sunrise(sunrise_time):
-    sunrise = parser.parse(sunrise_time)
-    start_decreasing_time = sunrise + datetime.timedelta(minutes=SUNRISE_OFFSET_MINUTES)
-    now = datetime.datetime.now()
-    return start_decreasing_time <= now < sunrise
-
 def parse_arguments():
+    """Parse command line arguments."""
     parser = argparse.ArgumentParser(description="Capture image script with test mode.")
     parser.add_argument("--test", help="Enable test mode.", action="store_true")
     return parser.parse_args()
 
 def load_config(config_path):
+    """Load configuration from a YAML file."""
     with open(config_path, 'r') as config_file:
         return yaml.safe_load(config_file)
 
+def load_sun_data(sun_data_file):
+    with open(sun_data_file, 'r') as file:
+        return json.load(file)
+
+
+def get_sun_times(date, sun_data):
+    """Get sunrise and sunset times for a given date."""
+    date_str = date.strftime('%m-%d')
+    return sun_data.get(date_str, {}).get('sunrise'), sun_data.get(date_str, {}).get('sunset')
+
+def is_after_sunset(sunset_time):
+    """Determine if the current time is after sunset."""
+    sunset = parser.parse(sunset_time)
+    now = datetime.datetime.now()
+    return now > sunset
+
+def is_transition_before_sunrise(sunrise_time):
+    """Determine if the current time is within the transition period before sunrise."""
+    sunrise = parser.parse(sunrise_time)
+    transition_start = sunrise + datetime.timedelta(minutes=SUNRISE_OFFSET_MINUTES)
+    now = datetime.datetime.now()
+    return transition_start <= now < sunrise
+
+def compute_shutter_increments():
+    """Compute the shutter speed increments for the transition period."""
+    return (MAX_SHUTTER - DAYTIME_SHUTTER) // 60  # 60 minutes for the transition
+
+def compute_gain_increments():
+    """Compute the gain increments for the transition period."""
+    return (MAX_GAIN - DAYTIME_GAIN) / 60.0  # 60 minutes for the transition, use floating point division
+
+def print_camera_config(camera_config, shutter_speed, gain):
+    """Print the current camera configuration in a table format."""
+    table = PrettyTable()
+    table.field_names = ["Setting", "Value"]
+    table.add_row(["Shutter Speed", shutter_speed])
+    table.add_row(["Gain", gain])
+    for key, value in camera_config['controls'].items():
+        table.add_row([key, value])
+    print(colored(table, 'green'))
+
 def setup_logging(config):
+    """Setup logging based on the provided configuration."""
     if config.get('log_capture_image'):
         log_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'logs')
         os.makedirs(log_dir, exist_ok=True)
@@ -53,9 +77,9 @@ def setup_logging(config):
         return False
 
 def load_camera_state():
+    """Load the camera state (shutter speed, gain) from a JSON file."""
     script_dir = os.path.dirname(os.path.realpath(__file__))
     camera_state_file = os.path.join(script_dir, 'data', 'camera_state.json')
-    
     try:
         with open(camera_state_file, "r") as file:
             state = json.load(file)
@@ -64,59 +88,20 @@ def load_camera_state():
         return DAYTIME_SHUTTER, max(1, DAYTIME_GAIN), 0, 0
 
 def save_camera_state(shutter_speed, gain, photo_counter, gain_increment_counter):
+    """Save the camera state (shutter speed, gain) to a JSON file."""
     script_dir = os.path.dirname(os.path.realpath(__file__))
     camera_state_file = os.path.join(script_dir, 'data', 'camera_state.json')
     with open(camera_state_file, "w") as file:
         json.dump({"shutter_speed": shutter_speed, "gain": gain, "photo_counter": photo_counter, "gain_increment_counter": gain_increment_counter}, file)
 
-
-def load_sun_data(sun_data_file):
-    with open(sun_data_file, 'r') as file:
-        return json.load(file)
-
-def get_sun_times(date, sun_data):
-    date_str = date.strftime('%m-%d')
-    return sun_data.get(date_str, {}).get('sunrise'), sun_data.get(date_str, {}).get('sunset')
-
-def is_start_of_night(sunset_time):
-    sunset = parser.parse(sunset_time).time()
-    now = datetime.datetime.now().time()
-    return sunset < now and (datetime.datetime.combine(datetime.date.today(), now) - datetime.datetime.combine(datetime.date.today(), sunset)).seconds <= 300
-
-def print_camera_config(camera_config, shutter_speed, gain):
-    table = PrettyTable()
-    table.field_names = ["Setting", "Value"]
-    table.add_row(["Shutter Speed", shutter_speed])
-    table.add_row(["Gain", gain])
-    for key, value in camera_config['controls'].items():
-        table.add_row([key, value])
-
-    print(colored(table, 'green'))
-
-def is_after_sunset(sunset_time):
-    sunset = parser.parse(sunset_time)
-    now = datetime.datetime.now()
-    return now > sunset
-
-def is_transition_before_sunrise(sunrise_time):
-    sunrise = parser.parse(sunrise_time)
-    transition_start = sunrise + datetime.timedelta(minutes=SUNRISE_OFFSET_MINUTES)
-    now = datetime.datetime.now()
-    return transition_start <= now < sunrise
-
-def compute_shutter_increments():
-    return (MAX_SHUTTER - DAYTIME_SHUTTER) // 60  # 60 minutes for the transition
-
-def compute_gain_increments():
-    return (MAX_GAIN - DAYTIME_GAIN) / 60.0  # 60 minutes for the transition, use floating point division
-
 def capture_night_image(config, logging_enabled, shutter_speed, gain, test_mode=False):
-    # Adjust camera settings for night time capturing
+    """Capture an image with night settings."""
     with Picamera2() as camera:
         # Set focus mode and lens position based on config
         focus_mode = libcamera.controls.AfModeEnum.Manual if config['focus_mode'] == 'manual' else libcamera.controls.AfModeEnum.Auto
         lens_position = config['lens_position'] if config['focus_mode'] == 'manual' else None
 
+        # Create the camera configuration
         camera_config = camera.create_still_configuration(
             main={"size": tuple(config['main_size'])},
             lores={"size": tuple(config['lores_size'])},
@@ -132,11 +117,7 @@ def capture_night_image(config, logging_enabled, shutter_speed, gain, test_mode=
             }
         )
         
-        if test_mode:
-            pass
-        
         print_camera_config(camera_config, shutter_speed, gain)
-            
         camera.options['quality'] = config['image_quality']
         camera.configure(camera_config)
 
@@ -146,6 +127,7 @@ def capture_night_image(config, logging_enabled, shutter_speed, gain, test_mode=
         camera.start()
         time.sleep(5)  # Allow the camera to adjust
 
+        # Determine the file name for the captured image
         if test_mode:
             file_name = os.path.join(config['image_output']['test_folder'], 'test.jpg')
         else:
@@ -154,8 +136,8 @@ def capture_night_image(config, logging_enabled, shutter_speed, gain, test_mode=
             os.makedirs(dir_name, exist_ok=True)
             file_name = os.path.join(dir_name, f"{config['image_output']['filename_prefix']}{now.strftime('%Y_%m_%d_%H_%M_%S')}.jpg")
 
+        # Capture the image and save it
         camera.capture_file(file_name)
-        # shutil.copy2(file_name, config['test_file'])
 
         if not test_mode and config['overlay']['enabled']:
             add_overlay(config, file_name)
@@ -170,13 +152,18 @@ def capture_night_image(config, logging_enabled, shutter_speed, gain, test_mode=
 
 if __name__ == "__main__":
     args = parse_arguments()
-
     script_dir = os.path.dirname(os.path.realpath(__file__))
     config = load_config(os.path.join(script_dir, 'config.yaml'))
+
+    # Load constants from config
+    DAYTIME_SHUTTER = config['camera_constants']['DAYTIME_SHUTTER']
+    DAYTIME_GAIN = config['camera_constants']['DAYTIME_GAIN']
+    MAX_SHUTTER = config['camera_constants']['MAX_SHUTTER']
+    MAX_GAIN = config['camera_constants']['MAX_GAIN']
+    SUNRISE_OFFSET_MINUTES = config['camera_constants']['SUNRISE_OFFSET_MINUTES']
+
     sun_data_file = os.path.join(script_dir, 'data', 'sun_data_2023.json')
     sun_data = load_sun_data(sun_data_file)
-
-    # Load sunset and sunrise times
     today = datetime.datetime.now().date()
     sunrise_time, sunset_time = get_sun_times(today, sun_data)
 
@@ -185,47 +172,37 @@ if __name__ == "__main__":
 
     shutter_speed, gain, photo_counter, gain_increment_counter = load_camera_state()
 
+    # Adjust shutter speed and gain based on current time relative to sunset/sunrise
     if is_after_sunset(sunset_time):
         print(f"Within transition period after sunset. Current shutter_speed: {shutter_speed}, gain: {gain}")
-        
         shutter_speed += compute_shutter_increments()
-
-        # Gain adjustment logic
         gain_increment = compute_gain_increments()
         gain_increment_counter += gain_increment
         if gain_increment_counter >= 1:
             gain += int(gain_increment_counter)
             gain_increment_counter -= int(gain_increment_counter)
-        print(f"GAIN: {gain}, compute_gain_increments(): {compute_gain_increments()}")
-
         if shutter_speed > MAX_SHUTTER:
             shutter_speed = MAX_SHUTTER
         if gain > MAX_GAIN:
             gain = MAX_GAIN
 
-    elif is_transition_before_sunrise(sunrise_time):  # Decrease settings before sunrise
+    elif is_transition_before_sunrise(sunrise_time):
         print(f"Within transition period before sunrise. Current shutter_speed: {shutter_speed}, gain: {gain}")
-        
         shutter_speed -= compute_shutter_increments()
-
-        # Gain adjustment logic
         gain_increment = compute_gain_increments()
         gain_increment_counter -= gain_increment
         if gain_increment_counter <= -1:
-            gain += int(gain_increment_counter)  # this will be a negative value
+            gain += int(gain_increment_counter)
             gain_increment_counter -= int(gain_increment_counter)
-
         if shutter_speed < DAYTIME_SHUTTER:
             shutter_speed = DAYTIME_SHUTTER
-        if gain < 1:  # Ensure gain is at least 1
+        if gain < 1:
             gain = 1
                 
     print(f"After adjustment. New shutter_speed: {shutter_speed}, gain: {gain}")
 
-    gain = int(gain)  # Convert to integer
-    shutter_speed = int(shutter_speed)  # Convert to integer
-
+    gain = int(gain)
+    shutter_speed = int(shutter_speed)
     logging_enabled = setup_logging(config)
     capture_night_image(config, logging_enabled, shutter_speed, gain, test_mode=args.test)
-
     save_camera_state(shutter_speed, gain, photo_counter, gain_increment_counter)
--- timelapse.py
+++ timelapse.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python
+
 import time
 import yaml
 import subprocess
@@ -6,42 +7,41 @@ from datetime import datetime, timedelta
 import json
 import os
 
-SUNRISE_OFFSET_MINUTES = -60  # Start transition 1 hour before actual sunrise
-
 def load_config(config_path):
+    """Load configuration from a YAML file."""
     with open(config_path, 'r') as config_file:
         return yaml.safe_load(config_file)
 
 def load_sun_data():
+    """Load sunrise and sunset data from a JSON file."""
     current_dir = os.path.dirname(os.path.realpath(__file__))
     sun_data_file = os.path.join(current_dir, 'data', 'sun_data_2023.json')
     with open(sun_data_file, 'r') as file:
         return json.load(file)
 
 def get_sun_times(date, sun_data):
+    """Retrieve sunrise and sunset times for a given date."""
     date_str = date.strftime('%m-%d')
     return sun_data.get(date_str, {}).get('sunrise'), sun_data.get(date_str, {}).get('sunset')
 
-def is_within_transition_period(sunrise_time, sunset_time):
-    sunrise = datetime.strptime(sunrise_time, '%H:%M') + timedelta(minutes=SUNRISE_OFFSET_MINUTES)
+def is_within_transition_period(sunrise_time, sunset_time, config):
+    """Check if current time is within the transition period of sunrise or sunset."""
+    sunrise = datetime.strptime(sunrise_time, '%H:%M')
     sunset = datetime.strptime(sunset_time, '%H:%M')
     now = datetime.now()
-    return sunset <= now <= sunset + timedelta(hours=2) or sunrise <= now <= datetime.strptime(sunrise_time, '%H:%M')
+    return sunset <= now <= sunset + timedelta(minutes=config['camera_constants']['POST_SUNSET_DELAY_MINUTES']) or sunrise <= now <= sunrise + timedelta(minutes=config['camera_constants']['SUNRISE_OFFSET_MINUTES'])
 
-def is_nighttime(sunrise_time, sunset_time):
-    sunset_end_transition = datetime.strptime(sunset_time, '%H:%M') + timedelta(hours=2)  # end of transition after sunset
-    sunrise_start_transition = datetime.strptime(sunrise_time, '%H:%M') + timedelta(minutes=SUNRISE_OFFSET_MINUTES)  # start of transition before sunrise
+def is_nighttime(sunrise_time, sunset_time, config):
+    """Determine if the current time is considered nighttime."""
+    sunset_end_transition = datetime.strptime(sunset_time, '%H:%M') + timedelta(minutes=config['camera_constants']['POST_SUNSET_DELAY_MINUTES'])
+    sunrise_end_transition = datetime.strptime(sunrise_time, '%H:%M') + timedelta(minutes=config['camera_constants']['SUNRISE_OFFSET_MINUTES'])
     now = datetime.now().time()
 
-    # Check if the time is after sunset's transition and before midnight
-    if sunset_end_transition.time() <= now:
-        return True
-    # Check if the time is after midnight and before sunrise's transition
-    elif now <= sunrise_start_transition.time():
-        return True
-    return False
+    # Check post-sunset and pre-sunrise-end-transition conditions
+    return sunset_end_transition.time() <= now or now <= sunrise_end_transition.time()
 
 def timelapse(config):
+    """Main function to run the timelapse, deciding which capture script to run based on current time and sun data."""
     sun_data = load_sun_data()
     interval = config['interval']
     current_dir = os.path.dirname(os.path.realpath(__file__))
@@ -50,25 +50,22 @@ def timelapse(config):
         today = datetime.now().date()
         sunrise_time, sunset_time = get_sun_times(today, sun_data)
         
-        # If there's "never_sets", just run the daytime script
         if sunrise_time == "never_sets" or sunset_time == "never_sets":
-            print("Running daytime script 1")
+            # Places where the sun doesn't set or rise
             subprocess.run(['python3', os.path.join(current_dir, 'capture_image.py')])
-        # If it's within the transition period, run the nighttime script
-        elif is_within_transition_period(sunrise_time, sunset_time):
-            print("Running nighttime script (transition)")
+        elif is_within_transition_period(sunrise_time, sunset_time, config):
+            # Transition periods (around sunrise and sunset)
             subprocess.run(['python3', os.path.join(current_dir, 'capture_image_night.py')])
-        # If it's nighttime, run the nighttime script
-        elif is_nighttime(sunrise_time, sunset_time):
-            print("Running nighttime script")
+        elif is_nighttime(sunrise_time, sunset_time, config):
+            # Nighttime
             subprocess.run(['python3', os.path.join(current_dir, 'capture_image_night.py')])
-        # Otherwise, run the daytime script
         else:
-            print("Running daytime script 2")
+            # Daytime
             subprocess.run(['python3', os.path.join(current_dir, 'capture_image.py')])
 
         time.sleep(interval)
 
 if __name__ == "__main__":
-    config = load_config(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'config.yaml'))
+    config_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'config.yaml')
+    config = load_config(config_path)
     timelapse(config)
